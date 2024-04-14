fa1bc495d662d40d48d0b547f6e15bfabd084597
sunnydcitruspunch <wiwisun18@gmail.com>
2024-04-14 12:43:40 -0600

    Remote extra buttons

--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -3,8 +3,6 @@
     <div class="title-bar">
       <div class="title-bar-text">Error</div>
       <div class="title-bar-controls">
-        <button aria-label="Minimize"></button>
-        <button aria-label="Maximize"></button>
         <button aria-label="Close"></button>
       </div>
     </div>
