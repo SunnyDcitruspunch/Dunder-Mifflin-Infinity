93a602c1b60b07073475ce72d1b253529d6a40cc
sunnydcitruspunch <wiwisun18@gmail.com>
2024-04-14 12:36:52 -0600

    Implement mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <img alt="dunder-mifflin-logo-bg" class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 align-middle leading-[100px] non-selectable" draggable="false" :src="wallpaper" style="height: 80vh" />
-  <ShortCut
-      v-for="shortcut in shortcuts"
-      :key="shortcut.name"
-      :img="shortcut.img"
-      :initialTop="shortcut.initialTop"
-      :name="shortcut.name"
-  />
-  <UnderConstruction />
+  <div v-if="isMobile">
+    <Mobile />
+  </div>
+  <div v-else>
+    <img alt="dunder-mifflin-logo-bg" class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 align-middle leading-[100px] non-selectable" draggable="false" :src="wallpaper" style="height: 80vh" />
+    <ShortCut
+        v-for="shortcut in shortcuts"
+        :key="shortcut.name"
+        :img="shortcut.img"
+        :initialTop="shortcut.initialTop"
+        :name="shortcut.name"
+    />
+    <UnderConstruction />
+  </div>
 </template>
 
 <script>
@@ -17,6 +22,7 @@ import computer from '@/assets/computer.png';
 import folder from '@/assets/folder.png';
 import wallpaper from '@/assets/dunder-mifflin-bg.png';
 import UnderConstruction from './components/UnderConstruction/UnderConstruction.vue';
+import Mobile from './components/Mobile.vue';
 import './common/reset.css'
 import './index.css'
 
@@ -24,8 +30,15 @@ export default {
   name: 'App',
   components: {
     // OfficeQuickSearch,
+    Mobile,
     UnderConstruction,
     ShortCut
+  },
+  computed: {
+    isMobile() {
+      console.log('window.innerWidth ', window.innerWidth)
+      return window.innerWidth <= 768;
+    }
   },
   data() {
     return {
@@ -52,7 +65,6 @@ export default {
   height: 100dvh;
   width: 100vw;
   background-color: #000FFF;
-  color: white;
   overflow: hidden;
   position: relative;
 }
