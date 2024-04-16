708a410d88c4dba48d43c0ecf40c54a60d7d4a3e
sunnydcitruspunch <wiwisun18@gmail.com>
2024-04-16 17:46:26 -0600

    Update styling

--- src/components/UnderConstruction/UnderConstruction.vue
+++ src/components/UnderConstruction/UnderConstruction.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white z-20 flex flex-col grow items-center m-1 mt-0 custom-font p-5">
-    <p class="text-black text-2xl font-bold mt-5">Under Construction</p>
+    <p class="text-black text-2xl font-bold mt-5 mb-5">Under Construction</p>
     <img alt="dunder-mifflin-paper-company" class="m-4 non-selectable" draggable="false" style="width: 150px" :src="logo" />
-    <img alt="jackhammer" class="w-1/2" :src="jackhammer" />
+    <img alt="jackhammer" class="w-80" :src="jackhammer" />
     <p class="font-bold text-xl">
       <span class="text-black">Coming </span>
       <span v-for="(char, index) in message" :key="index" :style="getColor(index)">
