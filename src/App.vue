e9a50df6e8c46f3510844e0746a985adfbc2abb2
sunnydcitruspunch <wiwisun18@gmail.com>
2024-04-15 19:08:30 -0600

    Update quick launch ui

--- src/App.vue
+++ src/App.vue
@@ -70,8 +70,12 @@ export default {
   overflow: hidden;
   position: relative;
 }
-body {
-  overflow: hidden;
+html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
 .non-selectable {
   user-select: none;
